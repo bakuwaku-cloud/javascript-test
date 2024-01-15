35d509a7cb0c895c9ad02355b8e917f779ad53f7
astro <angelica.strong0725@gmail.com>
2024-01-14 21:56:53 -0800

    added quiz questions

---
+++ assets/js/scripts.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question:
+            'Commonly used data types DO NOT include:',
+        answers: [
+            { text: '1. strings', correct: false },
+            { text: '2. booleans', correct: false },
+            { text: '3. alerts', correct: true },
+            { text: '4. numbers', correct: false },
+        ],
+    },
+    {
+        question:
+            'The condition in an if / else statement is enclosed within ____.',
+        answers: [
+            { text: '1. quotes', correct: false },
+            { text: '2. curly brackets', correct: false },
+            { text: '3. parentheses', correct: true },
+            { text: '4. square brackets', correct: false },
+        ],
+    },
+    {
+        question:
+            'Arrays in JavaScript can be used to store ____.',
+        answers: [
+            { text: '1. numbers and strings', correct: false },
+            { text: '2. other arrays', correct: false },
+            { text: '3. booleans', correct: false },
+            { text: '4. all of the above', correct: true },
+        ],
+    },
+    {
+        question:
+            'String values must be enclosed within ____ when being assigned to variables.',
+        answers: [
+            { text: '1. commas', correct: false },
+            { text: '2. curly brackets', correct: false },
+            { text: '3. quotes', correct: true },
+            { text: '4. parentheses', correct: false },
+        ],
+    },
+    {
+        question:
+            'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answers: [
+            { text: '1. JavaScript', correct: false },
+            { text: '2. terminal / bash', correct: false },
+            { text: '3. for loops', correct: false },
+            { text: '4. console.log', correct: true },
+        ],
+    },
+];
