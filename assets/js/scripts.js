18537760cff9154bcf45a90e12e02511c5a2e00d
astro <angelica.strong0725@gmail.com>
2024-01-14 22:29:56 -0800

    created function to dynamically append questions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -82,3 +82,17 @@ const questions = [
     },
 ];
 
+function showQuestion(question) {
+    resetState(); 
+    questionElement.innerText = question.question; 
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button); 
+    });
+}
